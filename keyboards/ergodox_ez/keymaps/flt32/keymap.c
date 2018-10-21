748b8a60c30d3e7a918bf1c7e690c951e2ce3610
hillah <hillah@gmail.com>
2018-10-21 20:26:13 +0900

    20181021

--- keyboards/ergodox_ez/keymaps/flt32/keymap.c
+++ keyboards/ergodox_ez/keymaps/flt32/keymap.c
@@ -47,12 +47,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | S- / \ |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl | Gui  | App  | Alt  | MO2  |                                       | Left | Down | Up   | Right| LCtrl |
+ *   |LCtrl | Gui  | App  | Alt  | MO2  |                                       | F2   | F3   | F4   | F5   |  \   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | F2   | F3   |       | F4   | F5   |
+ *                                        | F6   | F7   |       | F8   | F9   |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      | F6   |       | F7   |      |      |
+ *                                 |      |      | MHEN |       | KANA |      |      |
  *                                 | Space|MO3   |------|       |------|Back  |Enter |
  *                                 |      |      | Caps |       |Space |space |      |
  *                                 `--------------------'       `--------------------'
@@ -66,17 +66,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(KFNC),       KC_A,         KC_S,   KC_D,     KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,     KC_V,   KC_B,   JP_LBRC,
         KC_LCTRL,       KC_LGUI,      KC_APP, KC_LALT,  MO(MDIA),
-                                              KC_F2,  KC_F3,
-                                                               KC_F6,
+                                              KC_F6,  KC_F7,
+                                                               KC_MHEN,
                                                KC_SPC,MO(SYMB),LSFT(KC_CAPS),
         // right hand
              KC_DEL,      KC_6,   KC_7,    KC_8,   KC_9,   KC_0,           JP_MINS,
              KC_INS,      KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,           JP_AT,
                           KC_H,   KC_J,    KC_K,   KC_L,   JP_SCLN,        JP_COLN,
              JP_RBRC,     KC_N,   KC_M,    JP_COMM,JP_DOT, KC_SLSH,        SFT_T(JP_BSLS),
-                                  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,        KC_LCTRL,
-             KC_F4,          KC_F5,
-             KC_F7,
+                                  KC_F2,   KC_F3,  KC_F4,  KC_F5,          JP_YEN,
+             KC_F8,          KC_F9,
+             KC_KANA,
              KC_SPC,KC_BSPC, KC_ENT
     ),
 
